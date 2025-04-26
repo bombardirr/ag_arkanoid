2f4e797c5a6080d31911c092f3fa1794c5046e36
Artem Golovin <joe.falange@yandex.ru>
2025-04-26 11:05:13 +0300

    initial structure

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtRouteAnnouncer /> <!-- Для accessibility -->
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
